e3658c2504c4b83c02e32338542c80b32610ffb0
Jan-Matthis <mail@jan-matthis.de>
2020-06-30 09:16:26 +0200

    Fix citekey first word exclusion

--- papers/utils/bibtex.py
+++ papers/utils/bibtex.py
@@ -114,7 +114,7 @@ def make_citekey(lastname_first_author, year, title):
     for char in replace_char:
         title = title.replace(char, " ")
     for word in title.split(" "):
-        if word == not_first_word:
+        if word in not_first_word:
             continue
         else:
             title_word = word
